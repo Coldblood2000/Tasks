16a6967b1fbdf3a79b53e16d912be0eed2ce2567
dell <krushilunity18@gmail.com>
2020-02-25 06:39:33 +0530

    RadioBtn

--- destiny/src/main/java/com/krushil/destiny/MainActivity.java
+++ destiny/src/main/java/com/krushil/destiny/MainActivity.java
@@ -7,17 +7,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView signUpTxt;
     Button logInBtn;
-
+    Bundle bundle=null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Toast.makeText(this, "oncreate", Toast.LENGTH_SHORT).show();
         signUpTxt = (TextView)findViewById(R.id.signup_txt);
         logInBtn=(Button) findViewById(R.id.loginM_btn);
 
@@ -28,6 +29,11 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        bundle=getIntent().getExtras();
+        if (bundle!=null){
+            Toast.makeText(this, ""+bundle.getString("gender"), Toast.LENGTH_SHORT).show();
+        }
 
         logInBtn.setOnClickListener(new View.OnClickListener() {
             @Override
--- destiny/src/main/java/com/krushil/destiny/SignUpActivity.java
+++ destiny/src/main/java/com/krushil/destiny/SignUpActivity.java
@@ -6,24 +6,47 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.ImageButton;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 public class SignUpActivity extends AppCompatActivity {
 
     ImageButton backImgBtn;
-
+    Button next_btn;
+    CheckBox chkTermsAndConditions;
+    RadioButton radioMale,radioFemale;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
         backImgBtn=(ImageButton)findViewById(R.id.backSU_img_btn);
-        backImgBtn.setOnClickListener(new View.OnClickListener() {
+        chkTermsAndConditions=findViewById(R.id.checkBox);
+        radioMale=findViewById(R.id.radioMale);
+        radioFemale=findViewById(R.id.radioFemale);
+        next_btn=findViewById(R.id.next_btn);
+        next_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(SignUpActivity.this,MainActivity.class);
-                startActivity(intent);
-            }
+                if (chkTermsAndConditions.isChecked()) {
+                    Bundle bundle=new Bundle();
+                    if (radioMale.isChecked()){
+                        bundle.putString("gender","male");
+                    }
+                    else {
+                        bundle.putString("gender","female");
+                    }
+                    Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
+                    intent.putExtras(bundle);
+                    startActivity(intent);
+                }
+                else {
+                    Toast.makeText(SignUpActivity.this, "Please accept terms and conditions.", Toast.LENGTH_SHORT).show();
+                }
+                }
         });
+
     }
 }
